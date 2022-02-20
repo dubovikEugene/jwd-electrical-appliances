6bd8a870845be7f5af68c04d43a4146c8d2b2930
Eugene Dubovik <dev.dubovik@gmail.com>
2022-02-20 23:32:04 +0300

    add validate to Microvawe before creating Flat

--- src/com/epam/jwd/logic/FlatCreator.java
+++ src/com/epam/jwd/logic/FlatCreator.java
@@ -8,14 +8,15 @@ import com.epam.jwd.entity.enums.Brand;
 import com.epam.jwd.entity.enums.EnergyEfficiency;
 import com.epam.jwd.entity.enums.NoFrost;
 import com.epam.jwd.entity.flat.Flat;
-import com.epam.jwd.logic.validator.Validator;
 import com.epam.jwd.logic.validator.impl.ElectricKettleValidator;
 import com.epam.jwd.logic.validator.impl.FridgeValidator;
+import com.epam.jwd.logic.validator.impl.MicrowaveValidator;
 
 public class FlatCreator {
     private final Flat flat = new Flat();
     private final ElectricKettleValidator electricKettleValidator = new ElectricKettleValidator();
     private final FridgeValidator fridgeValidator = new FridgeValidator();
+    private final MicrowaveValidator microwaveValidator = new MicrowaveValidator();
 
     public Flat createFlat() {
         ElectricKettle firstElectricKettle = new ElectricKettle(1500,
@@ -49,20 +50,36 @@ public class FlatCreator {
                 true);
         fridgeValidator.validate(firstFridge);
 
-        flat.addAppliance(firstElectricKettle);
-        flat.addAppliance(secondElectricKettle);
-        flat.addAppliance(new Microwave(1200,
+        Microwave firstMicrowave = new Microwave(1200,
                 Brand.SAMSUNG,
                 12.0,
                 false,
-                25,
-                35));
-        flat.addAppliance(new Microwave(1750,
+                39,
+                26,
+                30,
+                true,
+                650,
+                true,
+                EnergyEfficiency.A_PLUS);
+        microwaveValidator.validate(firstMicrowave);
+
+        Microwave secondMicrowave = new Microwave(1750,
                 Brand.LG,
                 13.0,
                 true,
                 45,
-                30));
+                30,
+                30,
+                true,
+                650,
+                true,
+                EnergyEfficiency.A_PLUS);
+        microwaveValidator.validate(secondMicrowave);
+
+        flat.addAppliance(firstElectricKettle);
+        flat.addAppliance(secondElectricKettle);
+        flat.addAppliance(firstMicrowave);
+        flat.addAppliance(secondMicrowave);
         flat.addAppliance(new WashingMachine(1200,
                 Brand.ATLANT,
                 25.5,
