e7a3de174113bd9ab1a2d1d9e5b1100bf664694c
Eugene Dubovik <dev.dubovik@gmail.com>
2022-02-20 21:39:53 +0300

    check how is work

--- src/com/epam/jwd/App.java
+++ src/com/epam/jwd/App.java
@@ -3,7 +3,9 @@ package com.epam.jwd;
 import com.epam.jwd.comparator.AppliancesByPowerConsumptionComparator;
 import com.epam.jwd.entity.flat.Flat;
 import com.epam.jwd.logic.FlatCreator;
-import com.epam.jwd.logic.PowerConsumption;
+import com.epam.jwd.logic.PowerConsumptionInFlat;
+import com.epam.jwd.logic.search.Operator;
+import com.epam.jwd.logic.search.SearchAppliances;
 
 
 public class App {
@@ -12,8 +14,12 @@ public class App {
         Flat flat = flatCreator.createFlat();
 
         System.out.println(flat.toString() + "\n");
-        System.out.println(PowerConsumption.powerConsumptionEnabledAppliances(flat));
+        System.out.println(PowerConsumptionInFlat.powerConsumptionEnabledAppliances(flat));
         flat.getListAppliances().sort(new AppliancesByPowerConsumptionComparator());
         System.out.println(flat.toString());
+
+        SearchAppliances sa = new SearchAppliances();
+        System.out.println(sa.findBy(Operator.LARGER_THAN, 1200, flat));
+
     }
 }
