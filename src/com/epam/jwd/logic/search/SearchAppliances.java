3deae9a573bbbdb98afda87b5a624fe0841899f3
Eugene Dubovik <dev.dubovik@gmail.com>
2022-02-21 13:48:15 +0300

    add javadoc comments

--- src/com/epam/jwd/logic/search/SearchAppliances.java
+++ src/com/epam/jwd/logic/search/SearchAppliances.java
@@ -7,9 +7,27 @@ import com.epam.jwd.entity.flat.Flat;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class with searching methods
+ *
+ * @author Yauheni Dubovik
+ * {@link #findBy(Operator, int, Flat)}
+ * {@link #findBy(Brand, Flat)}
+ * {@link #findBy(Operator, double, Flat)}
+ */
 public class SearchAppliances {
-
-    public List<Appliances> findBy( Operator operator, int powerConsumption, Flat flat) {
+    /**
+     * This method find all Appliances in Flat by power consumption
+     * depending on the Operator
+     *
+     * @param operator         - is operator less, equal or larger
+     *                         from {@link Operator}
+     * @param powerConsumption - is power consumption by some Appliances
+     * @param flat             - is some Flat with difference Appliances
+     * @return List of Appliances who fits the given condition with
+     * Operator and powerConsumption
+     */
+    public List<Appliances> findBy(Operator operator, int powerConsumption, Flat flat) {
         List<Appliances> appliancesList = new ArrayList<>();
         switch (operator) {
             case EQUAL:
@@ -37,6 +55,15 @@ public class SearchAppliances {
         return appliancesList;
     }
 
+    /**
+     * This method find all Appliances in Flat by Brand
+     *
+     * @param brand - it`s brand by some Appliances
+     *              {@link  Brand}
+     * @param flat  - is some Flat with difference Appliances
+     * @return List of Appliances who fits the given condition
+     * with Brand
+     */
     public List<Appliances> findBy(Brand brand, Flat flat) {
         List<Appliances> appliancesList = new ArrayList<>();
         for (Appliances appliances : flat.getListAppliances()) {
@@ -47,6 +74,17 @@ public class SearchAppliances {
         return appliancesList;
     }
 
+    /**
+     * This method find all Appliances in Flat by weight
+     * depending on the Operator
+     *
+     * @param operator - is operator less, equal or larger
+     *                 from {@link Operator}
+     * @param weight   - is weight by some Appliances
+     * @param flat     - is some Flat with difference Appliances
+     * @return List of Appliances who fits the given condition with
+     * Operator and weight
+     */
     public List<Appliances> findBy(Operator operator, double weight, Flat flat) {
         List<Appliances> appliancesList = new ArrayList<>();
         switch (operator) {
