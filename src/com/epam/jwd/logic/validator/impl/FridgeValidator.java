95b5226584cb9f21206c2729e25d398f6f51cb16
Eugene Dubovik <dev.dubovik@gmail.com>
2022-02-21 13:22:12 +0300

    add javadoc comments

--- src/com/epam/jwd/logic/validator/impl/FridgeValidator.java
+++ src/com/epam/jwd/logic/validator/impl/FridgeValidator.java
@@ -3,7 +3,23 @@ package com.epam.jwd.logic.validator.impl;
 import com.epam.jwd.entity.appliances.Fridge;
 import com.epam.jwd.logic.validator.Validator;
 
+/**
+ * Class implement Validator parameterized by Fridge
+ * and check fields of Fridge to correctness
+ *
+ * @author Yauheni Dubovik
+ * @see Validator
+ */
 public class FridgeValidator implements Validator<Fridge> {
+
+    /**
+     * Checking fields to correctness data
+     *
+     * @param fridge some object type of Fridge
+     * @throws IllegalArgumentException if one of fields have
+     *                                  incorrect value
+     * @see Validator
+     */
     @Override
     public void validate(Fridge fridge) {
         validatePowerConsumption(fridge.getPowerConsumption());
@@ -11,9 +27,16 @@ public class FridgeValidator implements Validator<Fridge> {
         validateWidth(fridge.getWidth());
         validateHeight(fridge.getHeight());
         validateUsefulVolume(fridge.getUsefulVolume());
-
     }
 
+    /**
+     * The method checks that the power consumption is within the
+     * specified limits
+     *
+     * @param powerConsumption - is power consumption of Fridge
+     * @throws IllegalArgumentException if power consumption less than 5 W
+     *                                  or larger than 140 W
+     */
     private void validatePowerConsumption(int powerConsumption) {
         final int minPowerConsumption = 5;
         final int maxPowerConsumption = 140;
@@ -22,6 +45,13 @@ public class FridgeValidator implements Validator<Fridge> {
         }
     }
 
+    /**
+     * The method checks that the weight is within the specified limits
+     *
+     * @param weight - is weight by Fridge
+     * @throws IllegalArgumentException if weight less than 3.5 kg
+     *                                  or larger than 201.0 Kg
+     */
     private void validateWeight(double weight) {
         final double minWeight = 3.5;
         final double maxWeight = 201.0;
@@ -30,6 +60,13 @@ public class FridgeValidator implements Validator<Fridge> {
         }
     }
 
+    /**
+     * The method checks that the width is within the specified limits
+     *
+     * @param width - is widths by Fridge
+     * @throws IllegalArgumentException if width less than 25 cm
+     *                                  or larger than 204 cm
+     */
     private void validateWidth(int width) {
         final int minWidth = 25;
         final int maxWidth = 204;
@@ -38,6 +75,13 @@ public class FridgeValidator implements Validator<Fridge> {
         }
     }
 
+    /**
+     * The method checks that the height is within the specified limits
+     *
+     * @param height - is height by Fridge
+     * @throws IllegalArgumentException if height less than 30 cm
+     *                                  or larger than 221 cm
+     */
     private void validateHeight(int height) {
         final int minHeight = 30;
         final int maxHeight = 221;
@@ -46,6 +90,13 @@ public class FridgeValidator implements Validator<Fridge> {
         }
     }
 
+    /**
+     * The method checks that the useful volume is within the specified limits
+     *
+     * @param usefulVolume - is useful Volume by Fridge
+     * @throws IllegalArgumentException if useful Volume  less than 12 litres
+     *                                  or larger than 785 litres
+     */
     private void validateUsefulVolume(int usefulVolume) {
         final int minUsefulVolume = 12;
         final int maxUsefulVolume = 785;
