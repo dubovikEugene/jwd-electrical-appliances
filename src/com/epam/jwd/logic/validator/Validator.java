2d9ad9963948290caf58c1e165207e867c528f7d
Eugene Dubovik <dev.dubovik@gmail.com>
2022-02-21 13:05:17 +0300

    add javadoc comments

--- src/com/epam/jwd/logic/validator/Validator.java
+++ src/com/epam/jwd/logic/validator/Validator.java
@@ -1,7 +1,18 @@
 package com.epam.jwd.logic.validator;
 
-import com.epam.jwd.entity.appliances.Appliances;
-
+/**
+ * This interface is for validating the fields of a class of type <T>.
+ * It is necessary to check all possible fields for the correctness of the entered data
+ *
+ * @param <T> the type of objects that may be validated by this validator
+ * @author Yauheni Dubovik
+ */
 public interface Validator<T> {
-    public void validate(T t);
+
+    /**
+     * Checking the fields for the correctness of the entered data
+     *
+     * @param t some object of type <T>
+     */
+    void validate(T t);
 }
