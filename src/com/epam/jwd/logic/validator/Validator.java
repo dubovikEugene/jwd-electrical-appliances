93cd50636c4c9e4d7842db969f4ad26aa5a61d0b
Eugene Dubovik <dev.dubovik@gmail.com>
2022-02-21 13:13:38 +0300

    refactor javadoc comments

--- src/com/epam/jwd/logic/validator/Validator.java
+++ src/com/epam/jwd/logic/validator/Validator.java
@@ -1,7 +1,7 @@
 package com.epam.jwd.logic.validator;
 
 /**
- * This interface is for validating the fields of a class of type <T>.
+ * This interface is for validating the fields of a class of type T.
  * It is necessary to check all possible fields for the correctness of the entered data
  *
  * @param <T> the type of objects that may be validated by this validator
@@ -12,7 +12,7 @@ public interface Validator<T> {
     /**
      * Checking the fields for the correctness of the entered data
      *
-     * @param t some object of type <T>
+     * @param t some object of type T
      */
     void validate(T t);
 }
